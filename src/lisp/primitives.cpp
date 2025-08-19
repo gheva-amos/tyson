e3e0daaa7e098fad12796a539a391ba8d6338256
Spinoza's Ring <band@spinozasring.com>
2025-08-19 11:14:38 -0400

    Added the list primitive

--- src/lisp/primitives.cpp
+++ src/lisp/primitives.cpp
@@ -2,7 +2,7 @@
 
 void Env::load_primitives()
 {
-  define("ADD", Primitive{"ADD",
+  define("+", Primitive{"ADD",
     [](std::span<const Value> args) -> Value {
       double accumulator{0.0};
       for (auto& v : args)
@@ -19,7 +19,34 @@ void Env::load_primitives()
       return Value{Number{accumulator}};
     }
   });
-  define("MUL", Primitive{"NUL",
+  define("-", Primitive{"SUB",
+    [](std::span<const Value> args) -> Value {
+      double accumulator{0.0};
+      bool first{true};
+      for (auto& v : args)
+      {
+        if (first)
+        {
+          first = false;
+          accumulator = std::get<Number>(v).as_double();
+        }
+        if (std::holds_alternative<Number>(v))
+        {
+          accumulator -= std::get<Number>(v).as_double();
+        }
+        else
+        {
+          throw std::runtime_error("trying to subtract not a number");
+        }
+      }
+      if (args.size() == 1)
+      {
+        return Value{Number{-accumulator}};
+      }
+      return Value{Number{accumulator}};
+    }
+  });
+  define("*", Primitive{"NUL",
     [](std::span<const Value> args) -> Value {
       double accumulator{1.0};
       for (auto& v : args)
@@ -30,13 +57,13 @@ void Env::load_primitives()
         }
         else
         {
-          throw std::runtime_error("trying to add not a number");
+          throw std::runtime_error("trying to multiply not a number");
         }
       }
       return Value{Number{accumulator}};
     }
   });
-  define("DIV", Primitive{"DIV",
+  define("/", Primitive{"DIV",
     [](std::span<const Value> args) -> Value {
       double accumulator{std::get<Number>(args[0]).as_double()};
       for (size_t i{1}; i < args.size(); ++i)
@@ -48,10 +75,20 @@ void Env::load_primitives()
         }
         else
         {
-          throw std::runtime_error("trying to add not a number");
+          throw std::runtime_error("trying to devide not a number");
         }
       }
       return Value{Number{accumulator}};
+    }
+  });
+  define("list", Primitive{"LIST",
+    [](std::span<const Value> args) -> Value {
+      List ret;
+      for (auto v : args)
+      {
+        ret.push_back(v);
+      }
+      return ret;
     }
   });
 }
