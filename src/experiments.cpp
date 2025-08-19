3fdd620b8059bea360f721cf759fb66f45b4d57c
Spinoza's Ring <band@spinozasring.com>
2025-08-18 23:54:55 -0400

    Some promising first steps

--- src/experiments.cpp
+++ src/experiments.cpp
@@ -6,17 +6,85 @@
 int main()
 {
   std::unique_ptr<Env> environment = std::make_unique<Env>();
-  Nil nil;
-  environment->define("ADD", nil);
-  environment->define("MUL", nil);
-  environment->define("DIV", nil);
+  Primitive add, mul, div;
+  add.set_name("ADD");
+  add.set_function([](std::span<const Value> args) -> Value {
+    double accumulator{0.0};
+    for (auto& v : args)
+    {
+    std::cout << std::get<Number>(v) << std::endl;
+      if (std::holds_alternative<Number>(v))
+      {
+        accumulator += std::get<Number>(v).as_double();
+      }
+      else
+      {
+        throw std::runtime_error("trying to add not a number");
+      }
+    }
+    Number n;
+    n = accumulator;
+    Value ret;
+    ret = n;
+    return ret;
+  });
+  environment->define("ADD", add);
+  mul.set_name("MUL");
+  mul.set_function([](std::span<const Value> args) -> Value {
+    double accumulator{1.0};
+    for (auto& v : args)
+    {
+      if (std::holds_alternative<Number>(v))
+      {
+        accumulator *= std::get<Number>(v).as_double();
+      }
+      else
+      {
+        throw std::runtime_error("trying to add not a number");
+      }
+    }
+    Number n;
+    n = accumulator;
+    Value ret;
+    ret = n;
+    return ret;
+  });
+  environment->define("MUL", mul);
+  div.set_function([](std::span<const Value> args) -> Value {
+    double accumulator{std::get<Number>(args[0]).as_double()};
+    for (size_t i{1}; i < args.size(); ++i)
+    {
+      const Value& v{args[i]};
+      if (std::holds_alternative<Number>(v))
+      {
+        accumulator /= std::get<Number>(v).as_double();
+      }
+      else
+      {
+        throw std::runtime_error("trying to add not a number");
+      }
+    }
+    Number n;
+    n = accumulator;
+    Value ret;
+    ret = n;
+    return ret;
+  });
+  div.set_name("DIV");
+  environment->define("DIV", div);
+  std::string src2{R"END(
+(
+DIV 6 (MUL 1 3)
+)
+  )END" // should return 2
+  };
   std::string src{R"END(
 (
   ADD
-     (MUL 1 5)
+     (MUL 2 5)
      (DIV 6 3)
 )
-  )END"
+  )END" // should return 12
   };
   Parser p{src};
   auto parsed{p.parse()};
