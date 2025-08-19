cc8d4c4896a6818cc646d1874aacc2db0be76c05
Spinoza's Ring <band@spinozasring.com>
2025-08-19 13:00:18 -0400

    Adding the repl to work as our interactive lisp for testing

--- src/repl.cpp
+++ src/repl.cpp
@@ -2,12 +2,17 @@
 #include <string>
 #include <replxx.hxx>
 #include <utility>
+#include "parser/parser.h"
+#include "lisp/env.h"
 
 int main(int /*argc*/, char** /*argv*/)
 {
   replxx::Replxx console;
   console.set_max_history_size(10000);
   console.set_no_color(false);
+
+  std::unique_ptr<Env> environment = std::make_unique<Env>();
+
   while (true)
   {
     const char* cline = console.input(" >>> ");
@@ -21,9 +26,19 @@ int main(int /*argc*/, char** /*argv*/)
     {
       continue;
     }
-    console.history_add(line);
 
-    std::cout << line <<std::endl;
+    try
+    {
+      Parser p{line};
+      auto parsed{p.parse()};
+      auto val(parsed->eval(environment));
+      std::cout << val <<std::endl;
+      console.history_add(line);
+    }
+    catch (const std::runtime_error& err)
+    {
+      std::cout << err.what() << std::endl;
+    }
   }
   return 0;
 }
