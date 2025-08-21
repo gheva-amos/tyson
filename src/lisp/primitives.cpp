684d2d49d94f5876ba0027e65b419d7050d0fd30
Spinoza's Ring <band@spinozasring.com>
2025-08-21 14:33:15 -0400

    Using non const values for primitives

--- src/lisp/primitives.cpp
+++ src/lisp/primitives.cpp
@@ -5,7 +5,7 @@
 void Env::load_primitives()
 {
   define("+", Primitive{"ADD",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       double accumulator{0.0};
       for (auto& v : args)
       {
@@ -22,7 +22,7 @@ void Env::load_primitives()
     }
   });
   define("-", Primitive{"SUB",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       double accumulator{0.0};
       bool first{true};
       for (auto& v : args)
@@ -50,7 +50,7 @@ void Env::load_primitives()
     }
   });
   define("*", Primitive{"NUL",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       double accumulator{1.0};
       for (auto& v : args)
       {
@@ -67,11 +67,11 @@ void Env::load_primitives()
     }
   });
   define("/", Primitive{"DIV",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       double accumulator{args[0].as_number().as_double()};
       for (size_t i{1}; i < args.size(); ++i)
       {
-        const Value& v{args[i]};
+        Value& v{args[i]};
         if (v.is_number())
         {
           accumulator /= v.as_number().as_double();
@@ -85,7 +85,7 @@ void Env::load_primitives()
     }
   });
   define("list", Primitive{"LIST",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       List ret;
       for (auto v : args)
       {
@@ -95,8 +95,8 @@ void Env::load_primitives()
     }
   });
   define("car", Primitive{"CAR",
-    [](std::span<const Value> args) -> Value {
-      const Value& v{args[0]};
+    [](std::span<Value> args) -> Value {
+      Value& v{args[0]};
       if (args.size() != 1)
       {
         throw std::runtime_error("car works only on a single list");
@@ -110,8 +110,8 @@ void Env::load_primitives()
     }
   });
   define("cdr", Primitive{"CDR",
-    [](std::span<const Value> args) -> Value {
-      const Value& v{args[0]};
+    [](std::span<Value> args) -> Value {
+      Value& v{args[0]};
       if (args.size() != 1)
       {
         throw std::runtime_error("car works only on a single list");
@@ -125,7 +125,7 @@ void Env::load_primitives()
     }
   });
   define("print", Primitive{"PRINT",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       std::stringstream ss;
       for (auto& arg : args)
       {
@@ -135,7 +135,7 @@ void Env::load_primitives()
     }
   });
   define("cons", Primitive{"CONS",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       if (args.size() != 2)
       {
         throw std::runtime_error("cons works only on a 2 arguments");
@@ -161,7 +161,7 @@ void Env::load_primitives()
     }
   });
   define("<", Primitive{"LT",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       bool first{true};
       double current;
       for (auto& v : args)
@@ -187,7 +187,7 @@ void Env::load_primitives()
     }
   });
   define(">", Primitive{"GT",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       bool first{true};
       double current;
       for (auto& v : args)
@@ -213,7 +213,7 @@ void Env::load_primitives()
     }
   });
   define("=", Primitive{"EQ",
-    [](std::span<const Value> args) -> Value {
+    [](std::span<Value> args) -> Value {
       bool first{true};
       double current;
       for (auto& v : args)
